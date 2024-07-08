154c0823825826b1f02de189cdc18025165d687b
NadeeshaPiushan <nadeesha.piushan1996@gmail.com>
2024-07-08 17:03:02 +0530

    change the index.html file location

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000; // Port to listen on (default: 3000)
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Route for the main page ('/')
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
